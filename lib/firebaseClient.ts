417d63bf03f6157bf360880e17e5da055885839d
gurkankose1 <gurkankose1@gmail.com>
2025-11-10 15:18:50 +0300

    Create firebaseClient.ts

---
+++ lib/firebaseClient.ts
@@ -0,0 +1,16 @@
+// lib/firebaseClient.ts
+import { initializeApp, getApps } from "firebase/app";
+import { getAuth, GoogleAuthProvider } from "firebase/auth";
+
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY!,
+  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN!,
+  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID!,
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET!,
+};
+
+export const firebaseApp =
+  getApps().length ? getApps()[0]! : initializeApp(firebaseConfig);
+
+export const auth = getAuth(firebaseApp);
+export const googleProvider = new GoogleAuthProvider();
