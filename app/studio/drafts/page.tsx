e0bd2137ee3e6270d05ff54c5dab4c53151114fa
gurkankose1 <gurkankose1@gmail.com>
2025-11-11 07:26:54 +0300

    Update page.tsx

--- app/studio/drafts/page.tsx
+++ app/studio/drafts/page.tsx
@@ -1,6 +1,6 @@
 "use client";
 import { useEffect, useState } from "react";
-import { auth, googleProvider } from "../../lib/firebaseClient";
+import { auth, googleProvider } from "../../../lib/firebaseClient";
 import { signInWithPopup, onAuthStateChanged, signOut } from "firebase/auth";
 
 
