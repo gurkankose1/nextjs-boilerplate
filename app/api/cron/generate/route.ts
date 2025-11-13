bb07e2886493a4b7a69a8411650c458775839216
gurkankose1 <gurkankose1@gmail.com>
2025-11-13 22:08:37 +0300

    Update route.ts

--- app/api/cron/generate/route.ts
+++ app/api/cron/generate/route.ts
@@ -3,7 +3,7 @@ import { NextRequest } from "next/server";
 export const runtime = "nodejs";
 export const dynamic = "force-dynamic";
 
-import { adminDb } from "../../../lib/firebaseAdmin";
+import { adminDb } from "@/lib/firebaseAdmin";
 
 function baseUrlFrom(req: NextRequest) {
   const host =
