2661dca1fa4d96d4800bce4f4cf3e7fa5ffee581
gurkankose1 <gurkankose1@gmail.com>
2025-11-14 00:31:55 +0300

    Update route.ts

--- app/api/articles/view/route.ts
+++ app/api/articles/view/route.ts
@@ -1,20 +1,24 @@
 // app/api/articles/view/route.ts
 import { NextResponse } from "next/server";
 import { adminDb } from "@/lib/firebaseAdmin";
+import { FieldValue } from "firebase-admin/firestore";
 
 export async function POST(req: Request) {
   try {
     const { id } = await req.json();
 
     if (!id) {
-      return NextResponse.json({ ok: false, error: "id missing" }, { status: 400 });
+      return NextResponse.json(
+        { ok: false, error: "id missing" },
+        { status: 400 }
+      );
     }
 
     const ref = adminDb.collection("articles").doc(id);
 
     await ref.set(
       {
-        views: adminDb.FieldValue.increment(1),
+        views: FieldValue.increment(1),
       },
       { merge: true }
     );
