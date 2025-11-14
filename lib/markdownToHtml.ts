6ba7429f185f3de7bc3a2da9a12aad39d7546db0
gurkankose1 <gurkankose1@gmail.com>
2025-11-14 06:32:29 +0300

    Create markdownToHtml.ts

---
+++ lib/markdownToHtml.ts
@@ -0,0 +1,23 @@
+import { unified } from "unified";
+import remarkParse from "remark-parse";
+import remarkRehype from "remark-rehype";
+import rehypeSanitize from "rehype-sanitize";
+import rehypeSlug from "rehype-slug";
+import rehypeAutolinkHeadings from "rehype-autolink-headings";
+import rehypeStringify from "rehype-stringify";
+
+export async function markdownToHtml(markdown: string): Promise<string> {
+  const result = await unified()
+    .use(remarkParse)
+    .use(remarkRehype)
+    .use(rehypeSanitize)
+    .use(rehypeSlug)
+    .use(rehypeAutolinkHeadings, {
+      behavior: "wrap",
+      properties: { className: ["autolink"] },
+    })
+    .use(rehypeStringify)
+    .process(markdown);
+
+  return String(result);
+}
